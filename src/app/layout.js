c2eae5f5a8635a547bbb335b55cb458a19499423
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2024-07-25 01:44:50 +0530

    Completed Project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Google Clone",
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className="relative min-h-screen"
+      >
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
