12984d15b4b7b39da8b0300b6820c0b17e9cf4da
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2024-07-25 09:19:19 +0530

    inital commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
-
-
 export const metadata = {
   title: "Google Clone",
   description: "Open source google search clone",
 };
-
-{/* <meta name="google-site-verification" content="fMKcn7IOkwXMG5UuFI0e41yhaQrdpU9xz1fn-EUu7TM" /> */}
 
 export default function RootLayout({ children }) {
   return (
@@ -17,7 +13,6 @@ export default function RootLayout({ children }) {
       <body
         className="relative min-h-screen"
       >
-        {/* <meta name="google-site-verification" content="fMKcn7IOkwXMG5UuFI0e41yhaQrdpU9xz1fn-EUu7TM" /> */}
         {children}
         <Footer />
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,9 @@ import HomeHeader from "@/components/HomeHeader";
 import HomeSearch from "@/components/HomeSearch";
 import Image from "next/image";
 
-// export const meta = {
-//   name:"google-site-verification",
-//   content:"fMKcn7IOkwXMG5UuFI0e41yhaQrdpU9xz1fn-EUu7TM"
-// }
-
 export default function Home() {
   return (
     <div>
-      <meta name="google-site-verification" content="fMKcn7IOkwXMG5UuFI0e41yhaQrdpU9xz1fn-EUu7TM" />
       <HomeHeader />
       <div className="flex flex-col items-center mt-24" >
         <Image
