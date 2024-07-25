fc4027ade4914edf4f460e18cd38517149703116
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2024-07-25 08:27:04 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,8 @@ export const metadata = {
   description: "Open source google search clone",
 };
 
+<meta name="google-site-verification" content="fMKcn7IOkwXMG5UuFI0e41yhaQrdpU9xz1fn-EUu7TM" />
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
